4a4a092558ae0f2710feaf6ab0580c07c696f50c
Emer Arteaga <elias.arteaga@certicamara.com>
2023-08-07 18:59:54 -0500

    Se agrega loading

--- src/app/components/base/utilities/auth.interceptor.ts
+++ src/app/components/base/utilities/auth.interceptor.ts
@@ -9,15 +9,12 @@ export class ErrorInterceptor implements HttpInterceptor {
 constructor(private loadingService : LoadingService) {}
 
 intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.loadingService.setLoading(true);
     return next.handle(req)
         .pipe(catchError((err: HttpErrorResponse)=>{
-            this.loadingService.setLoading(false);
         return throwError(() => err);
         }),
         map((res: any) => {
             if(res?.body){
-                this.loadingService.setLoading(false);
                 return res;
             }
           }));
